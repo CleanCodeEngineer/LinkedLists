3fe7de50461056d84166318f2c38b4201f0149f7
Sahar Dastitash <sahar.dastitash@gmail.com>
2025-05-28 09:23:27 -0700

    Improved delete middle node in a Linked List

--- LinkedLists/deleteMiddleNode_Chapter2p3.cs
+++ LinkedLists/deleteMiddleNode_Chapter2p3.cs
@@ -1,62 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LinkedLists
+﻿namespace LinkedLists
 {
     public class deleteMiddleNode_Chapter2p3
     {
-        public static void deleteMiddleNode(LinkedListNode head)
+        // Time complexity: O(n)
+        // Space complexity: O(1)
+        public static LinkedListNode DeleteMiddleNode(LinkedListNode head)
         {
-            int count = 0;
-            var node = head;
-            int middleNode = 0;
+            if (head == null || head.Next == null)
+                return head; // Nothing to delete
 
-            while (node != null)
+            LinkedListNode slow = head;
+            LinkedListNode fast = head;
+            LinkedListNode prev = null;
+
+            // Use slow/fast pointer to find the middle
+            while (fast != null && fast.Next != null)
             {
-                count++;
-                node = node.Next;
+                fast = fast.Next.Next;
+                prev = slow;
+                slow = slow.Next;
             }
 
-            if (count <= 2)
-                return;
-            else if (count == 3)
-                middleNode = 2;
-            else if (count % 2 != 0)
-                middleNode = (count / 2) + 1;
-            else
-                middleNode = (count / 2);
+            // Delete the middle node
+            if (prev != null)
+                prev.Next = slow.Next;
 
-            LinkedListNode previousNode = null;
-            node = head;
-
-            for (int i = 0; i <= middleNode; i++)
-            {
-                //if (count % 2 != 0 && i == middleNode - 1)
-                //    previousNode = node;
-                //else if (count % 2 == 0 && i == middleNode - 2)
-                if (i == middleNode - 2)
-                {
-                    previousNode = node;
-                    node = node.Next;
-                }
-                else if (i == middleNode - 1)
-                {
-                    previousNode.Next = node.Next;
-                }
-                else
-                    node = node.Next;
-            }
-        }
-
-        public static void deleteMiddleNode1(LinkedListNode middleNode)
-        {
-            LinkedListNode node = new LinkedListNode(middleNode.Next.Value);
-            node.Next = middleNode.Next.Next;
-
-            middleNode = node;
+            return head;
         }
     }
 }
